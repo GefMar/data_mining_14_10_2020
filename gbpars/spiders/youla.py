f1058520fe2037af65ff845b9128bc35ed20662d
gefest <gefest@me.com>
2020-10-28 15:45:16 +0300

    lesson 4 yola

--- gbpars/spiders/youla.py
+++ gbpars/spiders/youla.py
@@ -1,3 +1,4 @@
+import re
 import scrapy
 from pymongo import MongoClient
 
@@ -25,8 +26,27 @@ class YoulaSpider(scrapy.Spider):
             yield response.follow(url, callback=self.ads_parse)
 
     def ads_parse(self, response, **kwargs):
+        specifications = {
+            itm.xpath('div[1]/text()').get(): itm.xpath('div[2]/text()').get() or itm.xpath('div[2]/a/text()').get() for
+            itm in response.xpath('//div[contains(@class, "AdvertCard_specs")]//div[contains(@class, "AdvertSpecs")]')
+            if itm.xpath('div[1]/text()').get()}
+
         name = response.xpath('//div[contains(@class, "AdvertCard_advertTitle")]/text()').extract_first()
         images = response.xpath('//div[contains(@class, "PhotoGallery_block")]//img/@src').extract()
+        autor = self.js_decoder_autor(response)
         # процедура соранения в БД
         collection = self.db_client['parse_10'][self.name]
-        collection.insert_one({'title': name, 'img': images})
+        collection.insert_one(
+            {'title': name,
+             'img': images,
+             'url': response.url,
+             'specifications': specifications,
+             'autor': autor,
+             }
+        )
+
+    def js_decoder_autor(self, response):
+        script = response.xpath('//script[contains(text(), "window.transitState =")]/text()').get()
+        re_str = re.compile(r"youlaId%22%2C%22([0-9|a-zA-Z]+)%22%2C%22avatar")
+        result = re.findall(re_str, script)
+        return f'https://youla.ru/user/{result[0]}' if result else None
