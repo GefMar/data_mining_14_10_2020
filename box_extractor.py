a50fd2def90f715dd72cc40049568cb85e5be07b
gefest <gefest@me.com>
2020-11-09 13:00:34 +0300

    lesson 8

---
+++ box_extractor.py
@@ -0,0 +1,47 @@
+import PyPDF2
+from PyPDF2.utils import PdfReadError
+from PIL import Image
+import pytesseract
+
+def extract_image(pdf_path):
+    results = []
+    with open(pdf_path, 'rb') as file:
+        try:
+            pdf_file = PyPDF2.PdfFileReader(file)
+        except PdfReadError as e:
+            print(e)
+        print(1)
+        for page in pdf_file.pages:
+            decoder = page['/Resources']['/XObject']['/Im0']['/Filter']
+
+            if decoder == '/DCTDecode':
+                file_type = 'jpg'
+            elif decoder == '/FlateDecode':
+                file_type = 'png'
+            elif decoder == '/JPXDecode':
+                file_type = 'jp2'
+            else:
+                file_type = 'bmp'
+            data = page['/Resources']['/XObject']['/Im0']._data
+            results.append((data, file_type))
+    return results
+
+
+def save_images(image_data, name):
+    with open(name, 'wb') as file:
+        file.write(image_data)
+
+
+def get_serial_number(file_path):
+    numbers = []
+    image = Image.open(file_path)
+    text = pytesseract.image_to_string(image, 'rus')
+    print(1)
+
+if __name__ == '__main__':
+    pdf_path = "/Users/gefest/projects/geekbrains/data_mining_14_10_2020/8416_4.pdf"
+    image = "/Users/gefest/projects/geekbrains/data_mining_14_10_2020/8416_1.jpg"
+    # images = extract_image(pdf_path)
+    # _ = [save_images(itm[0], f'{8416}_{idx}.{itm[1]}') for idx, itm in enumerate(images)]
+    get_serial_number(image)
+    print(1)
